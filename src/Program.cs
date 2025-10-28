c11852013b761bbc0ef1b025a883d5d13cf10c31
m4tc0des <matt_ufo2@hotmail.com>
2025-10-28 14:27:16 -0300

    Finalizado o desafio

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,18 @@
+using BugStore.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Recupera a string de conexão do appsettings.json
+var cnnString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// Registra o DbContext com SQLite
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(cnnString));
+
 var app = builder.Build();
 
+// Rota de teste
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
